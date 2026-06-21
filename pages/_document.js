ab2020c39d9bb06c7a20721d7975c95212f54de3
steal1224-sys <steal1224@gmail.com>
2026-06-22 08:54:16 +0900

    Include Pretendard stylesheet in _document.js
    Added a stylesheet link for Pretendard font.

--- pages/_document.js
+++ pages/_document.js
@@ -6,6 +6,12 @@ export default function Document() {
       <Head>
         <title>모란글샘 구입희망도서 신청</title>
         <meta name="description" content="부개여고 모란글샘 구입희망도서 신청" />
+    <link
+          rel="stylesheet"
+          as="style"
+          crossOrigin="anonymous"
+          href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/static/pretendard.css"
+        />
       </Head>
       <body>
         <Main />
