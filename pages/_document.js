457e59c6264230f361ff8ddb99a95dbe2133315d
steal1224-sys <steal1224@gmail.com>
2026-06-21 15:50:37 +0900

    Add custom Document component for Next.js

---
+++ pages/_document.js
@@ -0,0 +1,16 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="ko">
+      <Head>
+        <title>모란글샘 구입희망도서 신청</title>
+        <meta name="description" content="부개여고 모란글샘 구입희망도서 신청" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
